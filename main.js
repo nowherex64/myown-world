caab0776ebd0dfddbcc7d95ebe8e5cb8d2af04f4
nowherex64 <nowhere.hpp@gmail.com>
2024-04-29 15:56:32 -0500

    Se agrega toro

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 import * as THREE from 'three';
 import * as dat from 'lil-gui';
+import { DirectionalLight } from 'three';
+
+let toro = null;
 
 window.setup = (scene) => {
+    const axes = new THREE.AxesHelper(2);
+    const directionalLight = new DirectionalLight(0xffffff, 5);
+    directionalLight.position.set(2, 3, 1);
+
+    const material = new THREE.MeshToonMaterial({
+        color: 0xa13f4a,
+    });
+    toro = new THREE.Mesh(
+        new THREE.TorusGeometry(1, 0.5, 32, 32),
+        material
+    );
+
+    scene.add(axes);
+    scene.add(directionalLight);
+    scene.add(toro);
 };
 
 window.draw = (elapsedTime) => {
+    toro.rotation.x += 0.01;
+    toro.rotation.y += 0.015;
 };
