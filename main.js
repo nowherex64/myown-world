997d40c42c2ab8f275c18e96945d913e5e5d91db
nowherex64 <nowhere.hpp@gmail.com>
2024-04-29 16:19:46 -0500

    test

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ import * as dat from 'lil-gui';
 import { DirectionalLight } from 'three';
 
 let toro = null;
+const cursor = {
+    x: 0,
+    y: 0,
+};
 
 window.setup = (scene) => {
+
+    window.addEventListener('mousemove', (event) => {
+        cursor.x = (event.clientX / window.innerWidth) - 0.5;
+        cursor.y = (event.clientY / window.innerHeight) - 0.5;
+    });
+
     const axes = new THREE.AxesHelper(2);
     const directionalLight = new DirectionalLight(0xffffff, 5);
     directionalLight.position.set(2, 3, 1);
@@ -13,16 +23,20 @@ window.setup = (scene) => {
         color: 0xa13f4a,
     });
     toro = new THREE.Mesh(
-        new THREE.TorusGeometry(1, 0.5, 32, 32),
+        new THREE.TorusGeometry(1, 0.4, 32, 32),
         material
     );
+    toro.position.x = 1.5;
 
     scene.add(axes);
     scene.add(directionalLight);
     scene.add(toro);
 };
 
-window.draw = (elapsedTime) => {
-    toro.rotation.x += 0.01;
-    toro.rotation.y += 0.015;
+window.draw = (elapsedTime, camera, cameraGroup) => {
+    toro.rotation.x += 0.005;
+    toro.rotation.y += 0.0055;
+
+    cameraGroup.position.x += (cursor.x - cameraGroup.position.x)*0.1;
+    cameraGroup.position.y += (cursor.y - cameraGroup.position.x)*0.1;
 };
