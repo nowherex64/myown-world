7d618ff34bdfa92d8515180c56166a39c863a515
nowherex64 <nowhere.hpp@gmail.com>
2024-04-29 17:29:42 -0500

    Se agregan acciones al teclado

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ import * as dat from 'lil-gui';
 
 let esfera;
 let esferaFisica;
+const personaje = {
+    mesh: null,
+    fisica: null,
+};
 let world;
+const teclado = {
+    adelante: false,
+    atras: false,
+    derecha: false,
+    izquierda: false,
+};
+
+const velocidad = 0.05;
 
 window.setup = (scene) => {
 
@@ -25,8 +37,17 @@ window.setup = (scene) => {
 
     esfera = new THREE.Mesh(
         new THREE.SphereGeometry(1, 32, 32),
-        new THREE.MeshStandardMaterial({color: 0xa13f4a,})
+        material
     );
+
+    personaje.mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshToonMaterial({
+            color: 0x3f4aa1,
+        })
+    )
+    personaje.mesh.position.x = 3;
+    personaje.mesh.position.y = 0.5;
 
     esfera.position.y = 3;
     esfera.castShadow = true;
@@ -36,27 +57,34 @@ window.setup = (scene) => {
     scene.add(axes);
     scene.add(directionalLight);
     scene.add(esfera);
+    scene.add(personaje.mesh);
     scene.add(plano);
 
     world = new CANNON.World();
     world.gravity.set(0, -9.8, 0);
-    
+
     const eShape = new CANNON.Sphere(1);
     esferaFisica = new CANNON.Body({
         mass: 1,
-        position: CANNON.Vec3(0, 3, 0),
+        position: new CANNON.Vec3(0, 6, 0),
         shape: eShape,
     });
 
-    world.addBody(esferaFisica);
-    /*
-    const esferaBody = CANNON.Body({
-        mass: 1,
-        position: CANNON.Vec3(0, 3, 0),
-        shape: esferaShape,
+    const suelo = new CANNON.Body({
+        mass: 0,
+        position: new CANNON.Vec3(0, -1.0, 0),
+        shape: new CANNON.Box(new CANNON.Vec3(10, 1, 10))
     });
-    world.addBody(esferaBody);
-    */
+
+    personaje.fisica = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(3, 0.5, 0),
+        shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
+    });
+
+    world.addBody(esferaFisica);
+    world.addBody(suelo);
+    world.addBody(personaje.fisica);
 };
 
 window.draw = (elapsedTime) => {
@@ -65,4 +93,48 @@ window.draw = (elapsedTime) => {
     esfera.position.x = esferaFisica.position.x;
     esfera.position.y = esferaFisica.position.y;
     esfera.position.z = esferaFisica.position.z;
+
+    personaje.mesh.position.x = personaje.fisica.position.x;
+    personaje.mesh.position.y = personaje.fisica.position.y;
+    personaje.mesh.position.z = personaje.fisica.position.z;
+
+    if (teclado.adelante) {
+        personaje.fisica.position.x += velocidad;
+    }
+
+    if (teclado.atras) {
+        personaje.fisica.position.x -= velocidad;
+    }
+
+    if (teclado.derecha) {
+        personaje.fisica.position.z += velocidad;
+    }
+
+    if (teclado.izquierda) {
+        personaje.fisica.position.z -= velocidad;
+    }
 };
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w') {
+        teclado.adelante = true;
+    } else if (event.key === 's') {
+        teclado.atras = true;
+    } else if (event.key === 'a') {
+        teclado.izquierda = true;
+    } else if (event.key === 'd') {
+        teclado.derecha = true;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w') {
+        teclado.adelante = false;
+    } else if (event.key === 's') {
+        teclado.atras = false;
+    } else if (event.key === 'a') {
+        teclado.izquierda = false;
+    } else if (event.key === 'd') {
+        teclado.derecha = false;
+    }
+});
